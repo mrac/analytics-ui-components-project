14a15b4b1e22060e284567297ab66adc1305e038
Michal Raczkowski <michal.raczkowski@zalando.de>
2016-12-27 18:27:13 +0100

    use only UMD bundle

--- ng1-es5-amd/requirejs.config.js
+++ ng1-es5-amd/requirejs.config.js
@@ -1,5 +1,5 @@
 requirejs.config({
     paths: {
-        "analytics-ui-components": "node_modules/analytics-ui-components/dist/analytics-ui-components.amd"
+        "analytics-ui-components": "node_modules/analytics-ui-components/dist/analytics-ui-components"
     }
 });
