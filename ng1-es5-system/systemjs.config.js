3dd4346e745991ad04fe91f323dc9b5118300d56
Michal Raczkowski <michal.raczkowski@zalando.de>
2016-12-29 16:34:54 +0100

    upgraded for d3 ver.4

--- ng1-es5-system/systemjs.config.js
+++ ng1-es5-system/systemjs.config.js
@@ -10,6 +10,12 @@
         'analytics-ui-components': 'node_modules/analytics-ui-components/build/src'
     };
 
+    // map all d3 external packages to node_modules
+    var d3packages = ['d3', 'd3-array', 'd3-collection', 'd3-random', 'd3-ease', 'd3-polygon', 'd3-path', 'd3-quadtree', 'd3-queue', 'd3-shape', 'd3-color', 'd3-interpolate', 'd3-dispatch', 'd3-dsv', 'd3-request', 'd3-timer', 'd3-time', 'd3-format', 'd3-time-format', 'd3-scale', 'd3-selection', 'd3-transition', 'd3-axis', 'd3-hierarchy', 'd3-force', 'd3-drag', 'd3-voronoi', 'd3-zoom', 'd3-brush', 'd3-chord', 'd3-geo'];
+    d3packages.forEach(function (pck) {
+        map[pck] = 'node_modules/' + pck + '/build/' + pck + '.js';
+    });
+
     var packages = {
         'app': {
             main: 'index',
--- ng1-ts/rollup.config.js
+++ ng1-ts/rollup.config.js
@@ -1,24 +1,31 @@
-var rollupPluginIncludepaths = require('rollup-plugin-includepaths');
-var rollupPluginTypescript = require('rollup-plugin-typescript');
-var typescript = require('typescript');
+import rollupPluginIncludepaths from 'rollup-plugin-includepaths';
+import rollupPluginTypescript from 'rollup-plugin-typescript';
+import typescript from 'typescript';
 
-var includePathOptions = {
+
+const includePathOptions = {
     include: {},
     paths: [
         '.',
-        'node_modules/analytics-ui-components/src'
+        'node_modules/analytics-ui-components/src',
     ],
-    external: [
-    ],
-    extensions: ['.ts']
+    external: [],
+    extensions: ['.ts', '.js']
 };
 
-var typescriptOptions = {
+const typescriptOptions = {
     module: 'es6',
-    typescript: typescript
+    typescript: typescript,
+    paths: {}
 };
 
-var config = {
+
+// map all d3 external packages to node_modules
+const d3packages = ['d3', 'd3-array', 'd3-collection', 'd3-random', 'd3-ease', 'd3-polygon', 'd3-path', 'd3-quadtree', 'd3-queue', 'd3-shape', 'd3-color', 'd3-interpolate', 'd3-dispatch', 'd3-dsv', 'd3-request', 'd3-timer', 'd3-time', 'd3-format', 'd3-time-format', 'd3-scale', 'd3-selection', 'd3-transition', 'd3-axis', 'd3-hierarchy', 'd3-force', 'd3-drag', 'd3-voronoi', 'd3-zoom', 'd3-brush', 'd3-chord', 'd3-geo'];
+d3packages.forEach(pck => typescriptOptions.paths[pck] = ['node_modules/' + pck + '/index.js']);
+
+
+const config = {
     entry: 'src/index.ts',
     format: 'iife',
     dest: './build/index.js',
@@ -28,4 +35,4 @@ var config = {
     ]
 };
 
-module.exports = config;
+export default config;
